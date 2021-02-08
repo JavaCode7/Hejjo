8eac55eaabfd58b0c3e06a0d07f1fe4c6031d9b1
JavaCode7 <76915005+JavaCode7@users.noreply.github.com>
2021-02-08 14:14:13 +0000

    Hello Java, C and C++

---
+++ java/hello.java
@@ -0,0 +1,8 @@
+package java;
+// VScode is forcing me to specify a package name. I don't want to.
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.print("Hello World");        
+    }
+}
